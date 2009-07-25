acce06e155ae84effa81bba886cfc8f99bc1dca9
Luis Lavena <luislavena@gmail.com>
2009-07-25 17:36:53 -0300

    More polite with sudo warning message ;-)

--- sudo.bat
+++ sudo.bat
@@ -7,5 +7,5 @@ REM
 REM Save this file as 'sudo.bat' and place it somewhere in your PATH
 REM (ruby/bin is a good place too).
 REM
-ECHO.WARNING: You tried to use 'sudo' on Windows, check your scripts, please!
+ECHO.WARNING: You or one of your scripts tried to use 'sudo' on Windows. Please check your scripts!.
 CALL %*
